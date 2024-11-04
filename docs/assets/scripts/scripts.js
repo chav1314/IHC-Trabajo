0a99a440491f50a62404d133c6ff448272e7268a
chav1314 <107585872+chav1314@users.noreply.github.com>
2024-11-03 22:26:55 -0500

    Update scripts.js

--- docs/assets/scripts/scripts.js
+++ docs/assets/scripts/scripts.js
@@ -1,6 +1,35 @@
 let mainMap, routeMap, geocoder, directionsService, directionsRenderer, trafficLayer;
 let autocompleteStart, autocompleteDestination;
 
+const navLinks = document.querySelectorAll('.nav-links li a');
+navLinks.forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    link.style.fontSize = '18px';
+    link.style.fontWeight = 'bold';
+    link.style.color = '#000';
+    link.style.transition = 'font-size 0.3s ease, font-weight 0.3s ease, color 0.3s ease';
+  });
+  link.addEventListener('mouseleave', () => {
+    link.style.fontSize = '16px'; 
+    link.style.fontWeight = '600';
+    link.style.color = '#333';
+  });
+});
+const featureItems = document.querySelectorAll('.feature');
+
+featureItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    item.style.transform = 'scale(1.2)'; // Aumenta el tamaño un 10%
+    item.style.transition = 'transform 0.3s ease'; // Transición suave
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.style.transform = 'scale(1)'; // Restaura el tamaño original
+  });
+});
+
+//LOGICA DEL MAPA
+
 function initializeMap(elementId, options = {}) {
     const mapElement = document.getElementById(elementId);
     if (!mapElement) return null;
